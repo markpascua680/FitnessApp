6e3f9d8a3ea061758c9789e44d28f676d4ce46c6
Mark Pascua <markpascua680@yahoo.com>
2026-06-10 20:35:47 -0800

    Install Supabase client

--- FitnessApp/src/components/tabs/app-tabs.web.tsx
+++ FitnessApp/src/components/tabs/app-tabs.web.tsx
@@ -53,7 +53,7 @@ export function CustomTabList(props: TabListProps) {
     <View {...props} style={styles.tabListContainer}>
       <ThemedView type="backgroundElement" style={styles.innerContainer}>
         <ThemedText type="smallBold" style={styles.brandText}>
-          Fitness App
+          Final Form
         </ThemedText>
         {props.children}
       </ThemedView>
